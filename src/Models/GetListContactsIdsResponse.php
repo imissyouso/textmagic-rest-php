24f82d147a0490c6be0ada62fc16bccf326e8189
Andrey Vorobyev <andrey.v@textmagic.biz>
2019-09-16 08:31:34 +0000

    Auto commit

--- src/Models/GetListContactsIdsResponse.php
+++ src/Models/GetListContactsIdsResponse.php
@@ -186,7 +186,7 @@ class GetListContactsIdsResponse implements ModelInterface, ArrayAccess
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        $invalidProperties = [];
 
         return $invalidProperties;
     }
